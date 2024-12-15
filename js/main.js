a21c7d5deb6cc7352894f2d8b667b4bcd9607dbb
AndK37 <146865715+AndK37@users.noreply.github.com>
2024-12-15 08:47:17 +0500

    some pages

--- js/main.js
+++ js/main.js
@@ -20,3 +20,27 @@ function side() {
         b = !b;
     }
 }
+
+
+const dReg = document.getElementById("d-reg");
+const regLink = document.getElementsByClassName("header__link")
+
+regLink[0].addEventListener('click', () => {
+    dReg.showModal();
+});
+
+regLink[1].addEventListener('click', () => {
+    dReg.showModal();
+});
+
+const dLog = document.getElementById("d-log");
+const logBtn1 = document.getElementById("login-btn")
+const logBtn2 = document.getElementById("side-login-btn")
+
+logBtn1.addEventListener('click', () => {
+    dLog.showModal();
+});
+
+logBtn2.addEventListener('click', () => {
+    dLog.showModal();
+});
